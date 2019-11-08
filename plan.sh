39625874d3b03897a48eace669b5f850dd321025
Dharmonga <57536192+dharmonga@users.noreply.github.com>
2019-11-08 15:59:24 -0600

    Update plan.sh

--- plan.sh
+++ plan.sh
@@ -1,1 +1,8 @@
+$pkg_name="Roclink"
+$pkg_origin="Emerson"
+$pkg_verson="2.51"
+$pkg_maintainer="David Harmon <dharmon@enervest.net>"
+#$pkg-license=""
+#$pkg_source=""
+$pkg_filename="ROCLINK800.msi"
 
